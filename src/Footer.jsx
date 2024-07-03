f45b2db113ad7c5d4f3dace3dde367eed1d4d143
PrachiKarle <prachikkarle@gmail.com>
2024-07-03 22:35:51 +0530

    Second

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Footer = () => {
+  const [state,setState]=useState(false);
+  useEffect(()=>{
+    window.addEventListener('scroll',()=>{
+      if(window.scrollY > 80){
+        setState(true);
+      }
+      else{
+        setState(false);
+      }
+    })
+  })
   return (
     <>
       <div className="row m-0 p-5 text-light fw-bold text-center" style={{backgroundColor:"#24262A"}}>
@@ -10,6 +21,15 @@ const Footer = () => {
           Designed by{" "} <b >Prachi</b>
         </h6>
       </div>
+
+      {
+        state?
+        <div className="bg-primary d-flex justify-content-center align-items-center p-4
+        " style={{borderRadius:"50%",height:"20px" ,width:"30px",position:"fixed" ,bottom:"10px",right:"10px"}}>
+          <i className="bi bi-arrow-up text-light fw-bold"></i>
+        </div>:null
+      }
+      
     </>
   );
 };
